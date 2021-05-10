8b74cd23a6a3dc0bbf69ad48c662ec399866e91a
Anthony Carapetis <anthony.carapetis@ga.gov.au>
2021-05-10 15:20:39 +1000

    Allow unsupervised install with ./install.sh -y

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 
+SCRIPT_ARG="$1"
 NVIM_LINK="https://github.com/neovim/neovim/releases/download/v0.4.3/nvim.appimage"
 RG_API="https://api.github.com/repos/BurntSushi/ripgrep/releases"
 
@@ -7,30 +8,35 @@ cd "$(dirname "$0")"
 
 [ -d ~/.local/bin ] || mkdir -p ~/.local/bin
 which jq || curl https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 -o ~/.local/bin/jq
-ln -s "$PWD/json2tf" ~/.local/bin/
+ln -fs "$PWD/json2tf" ~/.local/bin/
+
+confirm() {
+    if [ "$SCRIPT_ARG" == "-y" ]; then
+        return 0
+    fi
+    read -p "$1 [Y/n]" o
+    [ -z "$o" ] || [ "${o^^}" == Y ]
+}
 
 if ! which nvim; then
-    read -p "nvim not found in path. Install appimage from github? [Y/n]" o
-    if [ -z "$o" ] || [ "${o^^}" == Y ]; then
+    if confirm "nvim not found in path. Install appimage from github?"; then
         [ ! -f nvim.appimage ] && curl -fL -o nvim.appimage --create-dirs $NVIM_LINK
         chmod +x nvim.appimage
         ./nvim.appimage --appimage-extract >/dev/null
         mv squashfs-root ~/.local/nvim
-        ln -s ~/.local/nvim/usr/bin/nvim ~/.local/bin/
+        ln -fs ~/.local/nvim/usr/bin/nvim ~/.local/bin/
         echo "Neovim installed to ~/.local/bin/nvim"
     fi
 fi
 
 if which nvim && ! which go; then
-    read -p "go not found in path. Install from yum/apt? (Needed for vim-hexokinase) [Y/n]" o
-    if [ -z "$o" ] || [ "${o^^}" == Y ]; then
+    if confirm "go not found in path. Install from yum/apt? (Needed for vim-hexokinase)"; then
         sudo yum install -y golang || sudo apt-get install -y golang
     fi
 fi
 
 if ! which rg; then
-    read -p "rg not found in path. Install binary from github? [Y/n]" o
-    if [ -z "$o" ] || [ "${o^^}" == Y ]; then
+    if confirm "rg not found in path. Install binary from github?"; then
         dl_url=$(curl $RG_API/latest | jq -r '.assets[]|select(.name|test("x86_64.*linux-musl.tar.gz"))|.browser_download_url')
         cd /tmp
         curl -fLo rg.tar.gz "$dl_url"
@@ -47,17 +53,17 @@ if [ ! -f ~/.local/share/nvim/site/autoload/plug.vim  ]; then
 fi
 
 [ ! -d ~/.config/nvim ] && mkdir -p ~/.config/nvim
-ln -s "$PWD/coc-settings.json" "$PWD/init.vim" ~/.config/nvim/ || true
+ln -fs "$PWD/coc-settings.json" "$PWD/init.vim" ~/.config/nvim/ || true
 for x in tmux.conf nethackrc pylintrc; do
-    ln -s "$PWD/$x" ~/.$x || true
+    ln -fs "$PWD/$x" ~/.$x || true
 done
 
 sudo pip3 -q install --upgrade pynvim jedi
 `which nvim` +PlugInstall +qall
-`which nvim` +"CocInstall coc-python" +"CocInstall coc-json" +"CocInstall coc-css" +"CocInstall coc-tsserver" +"CocInstall coc-html"
+`which nvim` +"CocInstall -sync coc-python coc-json coc-css coc-tsserver coc-html" +qall
 
 read -p "Add bashrc.inc to ~/.bashrc? [y/N]" o
-if [ "${o^^}" == Y ]; then
+if [ "$SCRIPT_ARG" == "-y" ] || [ "${o^^}" == Y ]; then
     echo '. "'"$PWD/bashrc.inc"'"' >> ~/.bashrc
 fi
 
