743f2e7b8044487dd0590c4b515a00fd1001db93
Anthony Carapetis <anthony.carapetis@gmail.com>
2024-06-02 15:31:49 +1000

    Add refactoring.nvim

---
+++ nvim/lua/plugins/refactoring.lua
@@ -0,0 +1,54 @@
+return {
+    {
+        "ThePrimeagen/refactoring.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-treesitter/nvim-treesitter",
+        },
+        config = function() require("refactoring").setup() end,
+        keys = {
+            {
+                "<leader>re",
+                mode = "x",
+                function() require("refactoring").refactor("Extract Function") end,
+                desc = "Extract Function",
+            },
+            {
+                "<leader>rf",
+                mode = "x",
+                function() require("refactoring").refactor("Extract Function To File") end,
+                desc = "Extract Function To File",
+            },
+            {
+                "<leader>rv",
+                mode = "x",
+                function() require("refactoring").refactor("Extract Variable") end,
+                desc = "Extract Variable",
+            },
+            {
+                "<leader>rI",
+                mode = "n",
+                function() require("refactoring").refactor("Inline Function") end,
+                desc = "Inline Function",
+            },
+            {
+                "<leader>ri",
+                mode = { "n", "x" },
+                function() require("refactoring").refactor("Inline Variable") end,
+                desc = "Inline Variable",
+            },
+            {
+                "<leader>rb",
+                mode = "n",
+                function() require("refactoring").refactor("Extract Block") end,
+                desc = "Extract Block",
+            },
+            {
+                "<leader>rbf",
+                mode = "n",
+                function() require("refactoring").refactor("Extract Block To File") end,
+                desc = "Extract Block To File",
+            },
+        },
+    },
+}
