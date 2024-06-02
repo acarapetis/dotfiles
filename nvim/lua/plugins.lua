698851cb22764587e4e581861b0eaed63c0e3275
Anthony Carapetis <anthony.carapetis@gmail.com>
2024-06-02 14:07:52 +1000

    Add nvim-treesitter highlighting

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -118,6 +118,40 @@ return {
     -- Completion
     { "neoclide/coc.nvim", branch = "release" },
 
+    -- Treesitter
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = {
+                    "c",
+                    "lua",
+                    "cpp",
+                    "rust",
+                    "python",
+                    "javascript",
+                    "typescript",
+                    "bash",
+                    "perl",
+                    "html",
+                    "xml",
+                    "json",
+                    "yaml",
+                    "toml",
+                    "sql",
+                    "proto",
+                    "vim",
+                    "vimdoc",
+                },
+                sync_install = false,
+                auto_install = true,
+                highlight = { enable = true, additional_vim_regex_highlighting = false },
+                -- indent = { enable = true },
+            })
+        end,
+    },
+
     -- Language support
     "Vimjas/vim-python-pep8-indent",
     {
