5317c75dce89d6859a8c94d747f95f60bbf91743
Anthony Carapetis <anthony.carapetis@gmail.com>
2024-06-03 00:10:31 +1000

    ditch gitsigns

--- nvim/lua/acarapetis/plugins/git.lua
+++ nvim/lua/acarapetis/plugins/git.lua
@@ -8,10 +8,4 @@ return {
         },
     },
     "tpope/vim-rhubarb",
-    {
-        "lewis6991/gitsigns.nvim",
-        config = function()
-            require('gitsigns').setup()
-        end
-    },
 }
