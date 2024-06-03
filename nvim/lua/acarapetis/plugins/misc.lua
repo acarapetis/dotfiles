35df89e28cb304eeaec72bfef056aad8fd4e9aa6
Anthony Carapetis <anthony.carapetis@ga.gov.au>
2024-06-03 15:39:31 +1000

    ui tweaks

--- nvim/lua/acarapetis/plugins/misc.lua
+++ nvim/lua/acarapetis/plugins/misc.lua
@@ -31,11 +31,6 @@ return {
         "lukas-reineke/indent-blankline.nvim",
         main = "ibl",
         opts = {
-            indent = { highlight = { "CursorColumn", "Whitespace" }, char = "" },
-            whitespace = {
-                highlight = { "CursorColumn", "Whitespace" },
-                remove_blankline_trail = false,
-            },
             scope = { enabled = false },
         },
     },
