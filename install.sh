255c3087ebd0a662a47013a9631b878e69f40243
Anthony Carapetis <anthony.carapetis@gmail.com>
2024-06-12 21:54:37 +1000

    clean up nvim, jq install

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ SCRIPT_ARG="$1"
 NVIM_LINK="https://github.com/neovim/neovim/releases/download/stable/nvim.appimage"
 RG_LINK="https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep-13.0.0-x86_64-unknown-linux-musl.tar.gz"
 UP_LINK="https://github.com/akavel/up/releases/download/v0.4/up"
+JQ_LINK="https://github.com/stedolan/jq/releases/download/jq-1.7.1/jq-linux64"
 
 cd "$(dirname "$0")"
 DOTFILES="$PWD"
@@ -40,14 +41,15 @@ tryinstall() {
 tryinstall python3-pip perl curl python3-pynvim
 tryinstall pspg || >&2 echo "Couldn't install pspg, continuing anyway"
 
-which jq || (curl -L https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 -o ~/.local/bin/jq && chmod +x ~/.local/bin/jq)
+which jq || (curl -L "$JQ_LINK" -o ~/.local/bin/jq && chmod +x ~/.local/bin/jq)
 
 if ! which nvim; then
     if confirm "nvim not found in path. Install appimage from github?"; then
-        [ ! -f nvim.appimage ] && curl -fL -o nvim.appimage --create-dirs $NVIM_LINK
+        curl -fL -o nvim.appimage --create-dirs "$NVIM_LINK"
         chmod +x nvim.appimage
         ./nvim.appimage --appimage-extract >/dev/null
         mv squashfs-root ~/.local/nvim
+        rm nvim.appimage
         ln -fs ~/.local/nvim/usr/bin/nvim ~/.local/bin/
         echo "Neovim installed to ~/.local/bin/nvim"
     fi
@@ -86,6 +88,8 @@ else
     echo "$bashrcinc" >> ~/.bashrc
 fi
 
+# We install fzf as an nvim plugin, but we actually want it available on the
+# CLI too, so use the built-in installer:
 ~/.local/share/nvim/lazy/fzf/install --all
 
 # Git config + aliases:
