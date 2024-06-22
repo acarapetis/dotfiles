20970e727c4223d62519ee8401bbb36527a2a7b0
Anthony Carapetis <anthony.carapetis@ga.gov.au>
2024-06-22 11:46:37 +1000

    nvim-cmp: use enter to confirm autoimports

--- config/nvim/lua/acarapetis/plugins/lsp.lua
+++ config/nvim/lua/acarapetis/plugins/lsp.lua
@@ -67,7 +67,7 @@ return {
                 mapping = cmp.mapping.preset.insert({
                     ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
                     ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-                    -- ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+                    ["<CR>"] = cmp.mapping.confirm({ select = false }),
                     ["<C-Space>"] = cmp.mapping.complete(),
                     ["<Tab>"] = cmp.mapping(function(fallback)
                         if cmp.visible() then
