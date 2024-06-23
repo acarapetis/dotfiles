1a942d6858e75d7aa037a1ad0257dfeef6f15e1c
Anthony Carapetis <anthony.carapetis@gmail.com>
2024-06-23 22:30:24 +1000

    nvim: comment out unused colorschemes

--- config/nvim/lua/acarapetis/plugins/colorschemes.lua
+++ config/nvim/lua/acarapetis/plugins/colorschemes.lua
@@ -18,25 +18,25 @@ return {
             })
         end,
     },
-    { "https://github.com/rebelot/kanagawa.nvim", lazy = true },
-    { "diegoulloao/neofusion.nvim", lazy = true },
-    { "oxfist/night-owl.nvim", lazy = true },
-    {
-        "rose-pine/neovim",
-        name = "rose-pine",
-        lazy = true,
-    },
-    { "catppuccin/nvim", name = "catppuccin", lazy = true },
-    {
-        "cocopon/iceberg.vim",
-        lazy = true,
-        config = function()
-            local white = "#f2f2f2"
-            vim.api.nvim_set_hl(0, "Normal", { fg = white })
-            vim.api.nvim_set_hl(0, "icebergNormalFg", { fg = white })
-            vim.api.nvim_set_hl(0, "DiffText", { fg = white })
-        end,
-    },
+    -- { "https://github.com/rebelot/kanagawa.nvim", lazy = true },
+    -- { "diegoulloao/neofusion.nvim", lazy = true },
+    -- { "oxfist/night-owl.nvim", lazy = true },
+    -- {
+    --     "rose-pine/neovim",
+    --     name = "rose-pine",
+    --     lazy = true,
+    -- },
+    -- { "catppuccin/nvim", name = "catppuccin", lazy = true },
+    -- {
+    --     "cocopon/iceberg.vim",
+    --     lazy = true,
+    --     config = function()
+    --         local white = "#f2f2f2"
+    --         vim.api.nvim_set_hl(0, "Normal", { fg = white })
+    --         vim.api.nvim_set_hl(0, "icebergNormalFg", { fg = white })
+    --         vim.api.nvim_set_hl(0, "DiffText", { fg = white })
+    --     end,
+    -- },
     {
         "nvim-lualine/lualine.nvim",
         dependencies = { "nvim-tree/nvim-web-devicons" },
