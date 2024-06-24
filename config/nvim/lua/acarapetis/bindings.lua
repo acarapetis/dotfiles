ba81ba45024f30e6999a851c6eaefe6361434600
Anthony Carapetis <anthony.carapetis@ga.gov.au>
2024-06-24 19:35:19 +1000

    tmux: alternate find-replace bind

--- config/nvim/lua/acarapetis/bindings.lua
+++ config/nvim/lua/acarapetis/bindings.lua
@@ -11,5 +11,6 @@ vim.keymap.set({"n", "v"}, "<leader>d", '"_d', { desc = "Delete without yank" })
 vim.keymap.set("n", "<C-j>", "<cmd>cnext<CR>zz", { desc = "Next quickfix" })
 vim.keymap.set("n", "<C-k>", "<cmd>cprev<CR>zz", { desc = "Prev quickfix" })
 
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Find and replace word under cursor" })
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Find and edit word under cursor" })
+vim.keymap.set("n", "<leader>S", [[:%s/\<<C-r><C-w>\>//gI<Left><Left><Left>]], { desc = "Find and replace word under cursor" })
 
