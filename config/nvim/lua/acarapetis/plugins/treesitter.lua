e55a9e918fab7a34f49dabeec1c3dd8f569e9e09
Anthony Carapetis <anthony.carapetis@gmail.com>
2024-06-29 23:57:12 +1000

    nvim: treesitter incremental_selection w/enter+tab

--- config/nvim/lua/acarapetis/plugins/treesitter.lua
+++ config/nvim/lua/acarapetis/plugins/treesitter.lua
@@ -35,6 +35,15 @@ return {
                     enable = true,
                     disable = { "python" },
                 },
+                incremental_selection = {
+                    enable = true,
+                    keymaps = {
+                        init_selection = "<CR>",
+                        scope_incremental = "<CR>",
+                        node_incremental = "<TAB>",
+                        node_decremental = "<S-TAB>",
+                    },
+                },
             })
         end,
     },
