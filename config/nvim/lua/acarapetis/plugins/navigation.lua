50ed2e4c99b5c2c9f9249b9d59568817dfbce069
Anthony Carapetis <anthony.carapetis@gmail.com>
2024-06-30 17:04:49 +1000

    nvim: Add icon padding to fzf-lua
    Fixes cut-off icons in st

--- config/nvim/lua/acarapetis/plugins/navigation.lua
+++ config/nvim/lua/acarapetis/plugins/navigation.lua
@@ -23,26 +23,27 @@ return {
             "nvim-tree/nvim-web-devicons",
         },
         keys = {
-            { "<C-p>", function() require("fzf-lua").files() end, desc = "by filename" },
-            { "<C-f>", function() require("fzf-lua").live_grep_native() end, desc = "grep" },
-            { "<C-g>", function() require("fzf-lua").grep_cword() end, desc = "word under cursor" },
-            { "<C-b>", function() require("fzf-lua").buffers() end, desc = "buffers" },
-            { "<BS>", function() require("fzf-lua").oldfiles() end, desc = "history" },
-            { "<Leader>fp", function() require("fzf-lua").files() end, desc = "by filename" },
-            { "<Leader>ff", function() require("fzf-lua").live_grep_native() end, desc = "grep" },
-            { "<Leader>fw", function() require("fzf-lua").grep_cword() end, desc = "word under cursor" },
-            { "<Leader>fW", function() require("fzf-lua").grep_cword() end, desc = "WORD under cursor" },
-            { "<Leader>fb", function() require("fzf-lua").buffers() end, desc = "buffers" },
-            { "<Leader>fh", function() require("fzf-lua").oldfiles() end, desc = "history" },
-            { "<Leader>fg", function() require("fzf-lua").git_status() end, desc = "git status"},
-            { "<Leader>fs", function() require("fzf-lua").lsp_document_symbols() end, desc = "symbols in document" },
-            { "<Leader>fS", function() require("fzf-lua").lsp_live_workspace_symbols() end, desc = "symbols in workspace" },
-            { "<Leader>fr", function() require("fzf-lua").lsp_references() end, desc = "references to word under cursor" },
-            { "<Leader>fc", function() require("fzf-lua").commands() end, desc = "nvim commands" },
-            { "<Leader><F1>", function() require("fzf-lua").helptags() end, desc = "nvim help" },
+            { "<C-p>",        function() require("fzf-lua").files() end,                      desc = "by filename" },
+            { "<C-f>",        function() require("fzf-lua").live_grep_native() end,           desc = "grep" },
+            { "<C-g>",        function() require("fzf-lua").grep_cword() end,                 desc = "word under cursor" },
+            { "<C-b>",        function() require("fzf-lua").buffers() end,                    desc = "buffers" },
+            { "<BS>",         function() require("fzf-lua").oldfiles() end,                   desc = "history" },
+            { "<Leader>fp",   function() require("fzf-lua").files() end,                      desc = "by filename" },
+            { "<Leader>ff",   function() require("fzf-lua").live_grep_native() end,           desc = "grep" },
+            { "<Leader>fw",   function() require("fzf-lua").grep_cword() end,                 desc = "word under cursor" },
+            { "<Leader>fW",   function() require("fzf-lua").grep_cword() end,                 desc = "WORD under cursor" },
+            { "<Leader>fb",   function() require("fzf-lua").buffers() end,                    desc = "buffers" },
+            { "<Leader>fh",   function() require("fzf-lua").oldfiles() end,                   desc = "history" },
+            { "<Leader>fg",   function() require("fzf-lua").git_status() end,                 desc = "git status" },
+            { "<Leader>fs",   function() require("fzf-lua").lsp_document_symbols() end,       desc = "symbols in document" },
+            { "<Leader>fS",   function() require("fzf-lua").lsp_live_workspace_symbols() end, desc = "symbols in workspace" },
+            { "<Leader>fr",   function() require("fzf-lua").lsp_references() end,             desc = "references to word under cursor" },
+            { "<Leader>fc",   function() require("fzf-lua").commands() end,                   desc = "nvim commands" },
+            { "<Leader><F1>", function() require("fzf-lua").helptags() end,                   desc = "nvim help" },
         },
         opts = {
-            fzf_opts = {["--layout"] = "default"},
+            fzf_opts = { ["--layout"] = "default" },
+            file_icon_padding = " ",
             oldfiles = {
                 include_current_session = true,
             }
@@ -51,7 +52,7 @@ return {
     {
         "mbbill/undotree",
         keys = {
-            { "<leader>u", vim.cmd.UndotreeToggle, desc="UndoTree" }
+            { "<leader>u", vim.cmd.UndotreeToggle, desc = "UndoTree" }
         },
     }
 }
