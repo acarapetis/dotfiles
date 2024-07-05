4858f279f57bc953b044778912f2040afcd15626
Anthony Carapetis <anthony.carapetis@ga.gov.au>
2024-07-05 11:26:40 +1000

    Add nvim-colorizer

--- config/nvim/lua/acarapetis/plugins/misc.lua
+++ config/nvim/lua/acarapetis/plugins/misc.lua
@@ -68,4 +68,11 @@ return {
             }
         },
     },
+    {
+        "norcalli/nvim-colorizer.lua",
+        config = function()
+            vim.o.termguicolors = true
+            require("colorizer").setup()
+        end
+    }
 }
