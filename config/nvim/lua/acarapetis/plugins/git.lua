3c6e28064884354a2354c508847cc6512d400ffe
Anthony Carapetis <anthony.carapetis@ga.gov.au>
2024-07-08 10:22:49 +1000

    nvim: gb to browse in github/bitbucket

--- config/nvim/lua/acarapetis/plugins/git.lua
+++ config/nvim/lua/acarapetis/plugins/git.lua
@@ -3,9 +3,18 @@ return {
         "tpope/vim-fugitive",
         lazy = false,
         keys = {
-            { "<leader>G", ":G", desc="Git command" },
-            { "<leader>g", vim.cmd.G, desc="Git status" },
+            { "<leader>G", ":G",            desc = "Git command" },
+            { "<leader>g", vim.cmd.G,       desc = "Git status" },
+            { "gb",        vim.cmd.GBrowse, desc = "View on GitHub/Bitbucket" },
         },
+        config = function()
+            vim.api.nvim_create_user_command(
+                "Browse",
+                function(opts) vim.fn.system({ "xdg-open", opts.fargs[1] }) end,
+                { nargs = 1 }
+            )
+        end,
     },
     "tpope/vim-rhubarb",
+    "tommcdo/vim-fubitive",
 }
