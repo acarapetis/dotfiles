884eaae96c1961e84918e2af55e05e106ec9bae6
Anthony Carapetis <anthony.carapetis@ga.gov.au>
2024-10-24 14:52:20 +1100

    nvim: Autoformat on write in python and tf
    taking the plunge...

--- config/nvim/lua/acarapetis/plugins/reformat.lua
+++ config/nvim/lua/acarapetis/plugins/reformat.lua
@@ -1,14 +1,15 @@
 return {
     {
         "stevearc/conform.nvim",
+        event = "VeryLazy",
         config = function()
             local prettier = { "prettierd", "prettier", stop_after_first = true }
             require("conform").setup({
                 formatters_by_ft = {
-                    lua = { "stylua" },
                     python = { "ruff_fix", "ruff_format" },
-                    rust = { "rustfmt", lsp_format = "fallback" },
                     terraform = { "terraform_fmt" },
+                    lua = { "stylua" },
+                    rust = { "rustfmt", lsp_format = "fallback" },
                     javascript = prettier,
                     javascriptreact = prettier,
                     typescript = prettier,
@@ -19,6 +20,15 @@ return {
                     css = prettier,
                     markdown = prettier,
                 },
+            })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                pattern = "*",
+                callback = function(args)
+                    local ft = vim.bo[args.buf].filetype
+                    if ft == "python" or ft == "terraform" then
+                        require("conform").format({ bufnr = args.buf })
+                    end
+                end,
             })
         end,
         keys = {
