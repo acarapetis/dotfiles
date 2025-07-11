ac0ddf5305194130470e65d27a851336da8c178e
Anthony Carapetis <anthony.carapetis@ga.gov.au>
2025-07-11 13:40:31 +1000

    drop rainbow-delimiters
    really slows down in big files, and don't really feel like I get much
    value from it

--- config/nvim/lua/acarapetis/plugins/treesitter.lua
+++ config/nvim/lua/acarapetis/plugins/treesitter.lua
@@ -109,21 +109,6 @@ return {
         end,
     },
     {
-        "hiphish/rainbow-delimiters.nvim",
-        config = function()
-            local rainbow_delimiters = require("rainbow-delimiters")
-            require("rainbow-delimiters.setup").setup({
-                strategy = {
-                    [""] = rainbow_delimiters.strategy["local"],
-                },
-                query = {
-                    [""] = "rainbow-delimiters",
-                    --lua = "rainbow-blocks",
-                },
-            })
-        end,
-    },
-    {
         "nvim-treesitter/nvim-treesitter-context",
         lazy = false,
         config = function()
