eeacc7158de82fa6c45e58efddf32f2652141a8e
Anthony Carapetis <anthony.carapetis@ga.gov.au>
2025-08-01 10:10:26 +1000

    nvim: highlight tree_sitter queries in python

--- config/nvim/after/queries/python/injections.scm
+++ config/nvim/after/queries/python/injections.scm
@@ -1,6 +1,14 @@
 ;; extends
 (
     (string_content) @injection.content
-    (#any-match? @injection.content "^\\/\\*SQL\\*\\/")
+    (#match? @injection.content "^\\/\\*SQL\\*\\/")
     (#set! injection.language "sql")
 )
+
+; inline queries in tree_sitter:
+(call 
+  function: (identifier) @callable
+  arguments: (argument_list (_) (string (string_content) @injection.content))
+  (#eq? @callable "Query")
+  (#set! injection.language "query")
+
