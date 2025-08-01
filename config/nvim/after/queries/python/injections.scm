9ad4eed7425881a1bf9afa19a7e552cc09d5105b
Anthony Carapetis <anthony.carapetis@ga.gov.au>
2025-08-01 10:10:26 +1000

    nvim: ts injection for op.execute(SQL)

--- config/nvim/after/queries/python/injections.scm
+++ config/nvim/after/queries/python/injections.scm
@@ -1,14 +1,24 @@
 ;; extends
+
+; explicitly tagged sql strings
 (
     (string_content) @injection.content
     (#match? @injection.content "^\\/\\*SQL\\*\\/")
     (#set! injection.language "sql")
 )
 
+; literal sql in alembic migrations:
+(call 
+  function: (attribute (identifier) @op (identifier) @execute) @callable
+  arguments: (argument_list . (string (string_content) @injection.content))
+  (#eq? @op "op")
+  (#eq? @execute "execute")
+  (#set! injection.language "sql"))
+
 ; inline queries in tree_sitter:
 (call 
   function: (identifier) @callable
   arguments: (argument_list (_) (string (string_content) @injection.content))
   (#eq? @callable "Query")
-  (#set! injection.language "query")
+  (#set! injection.language "query"))
 
