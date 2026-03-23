74c644f4c25dfc2ed1e775a4a14efd5dee15fd57
Anthony Carapetis <anthony.carapetis@gmail.com>
2026-03-23 16:55:24 +1100

    Update to nvim-treesitter main branch

--- config/nvim/lua/acarapetis/plugins/treesitter.lua
+++ config/nvim/lua/acarapetis/plugins/treesitter.lua
@@ -1,123 +1,125 @@
----@diagnostic disable: missing-fields
 return {
-    { "nvim-treesitter/nvim-treesitter-textobjects", branch = "master" },
+    { "nvim-treesitter/nvim-treesitter-textobjects", branch = "main" },
     {
         "nvim-treesitter/nvim-treesitter",
-        branch = "master",
-        dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
+        branch = "main",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter-textobjects",
+            {
+                "acarapetis/nvim-treesitter-jjconfig",
+                branch = "main",
+                lazy = false,
+                opts = {},
+            },
+        },
         build = ":TSUpdate",
         config = function()
-            require("nvim-treesitter.configs").setup({
-                ensure_installed = {
-                    "c",
-                    "lua",
-                    "cpp",
-                    "rust",
-                    "python",
-                    "javascript",
-                    "typescript",
-                    "bash",
-                    "perl",
-                    "html",
-                    "xml",
-                    "json",
-                    "yaml",
-                    "toml",
-                    "sql",
-                    "proto",
-                    "vim",
-                    "vimdoc",
-                    "regex",
-                    "styled",
-                },
-                sync_install = false,
-                auto_install = true,
-                highlight = {
-                    enable = true,
-                    additional_vim_regex_highlighting = false,
-                    disable = { "tmux", "json", "xml", "csv", "jinja" },
-                },
-                indent = {
-                    enable = true,
-                    disable = { "json", "xml", "csv", "python" },
-                },
-                incremental_selection = {
-                    enable = true,
-                    keymaps = {
-                        init_selection = "<CR>",
-                        scope_incremental = "<CR>",
-                        node_incremental = "<TAB>",
-                        node_decremental = "<S-TAB>",
-                    },
-                },
-                textobjects = {
-                    select = {
-                        enable = true,
+            local ts = require("nvim-treesitter")
 
-                        -- Automatically jump forward to textobj, similar to targets.vim
-                        lookahead = true,
-
-                        keymaps = {
-                            -- You can use the capture groups defined in textobjects.scm
-                            ["af"] = "@function.outer",
-                            ["if"] = "@function.inner",
-                            ["ac"] = "@class.outer",
-                            ["ic"] = "@class.inner",
-                            -- You can also use captures from other query groups like `locals.scm`
-                            ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
-                        },
-                        selection_modes = {
-                            ["@parameter.outer"] = "v",
-                            ["@function.outer"] = "V",
-                            ["@class.outer"] = "V",
-                        },
-                        include_surrounding_whitespace = false,
-                    },
-                    swap = {
-                        enable = true,
-                        swap_next = {
-                            ["<leader>a"] = "@parameter.inner",
-                        },
-                        swap_previous = {
-                            ["<leader>A"] = "@parameter.inner",
-                        },
-                    },
-                    move = {
-                        enable = true,
-                        set_jumps = true, -- whether to set jumps in the jumplist
-                        goto_next_start = {
-                            ["]f"] = "@function.outer",
-                            ["]c"] = "@class.outer",
-                        },
-                        goto_next_end = {
-                            ["]F"] = "@function.outer",
-                            ["]C"] = "@class.outer",
-                        },
-                        goto_previous_start = {
-                            ["[f"] = "@function.outer",
-                            ["[c"] = "@class.outer",
-                        },
-                        goto_previous_end = {
-                            ["[F"] = "@function.outer",
-                            ["[C"] = "@class.outer",
-                        },
-                    },
-                },
+            -- Install parsers that were previously in ensure_installed.
+            -- auto_install is gone in the main branch; parsers must be installed
+            -- explicitly (or on demand via :TSInstall). This runs async at startup.
+            ts.install({
+                "c",
+                "lua",
+                "cpp",
+                "rust",
+                "python",
+                "javascript",
+                "typescript",
+                "bash",
+                "perl",
+                "html",
+                "xml",
+                "json",
+                "yaml",
+                "toml",
+                "sql",
+                "proto",
+                "vim",
+                "vimdoc",
+                "regex",
+                "styled",
+                "jjconfig",
+                "jjrevset",
+                "jjtemplate",
+                "jjui",
             })
 
-            vim.cmd([[
-                set foldmethod=expr
-                set foldexpr=nvim_treesitter#foldexpr()
-                set nofoldenable " Disable folding at startup.
-                set foldlevelstart=99 " When folding is enabled, start with all folds open.
-            ]])
+            local highlight_disable = { tmux = true, json = true, xml = true, csv = true, jinja = true }
+            local indent_disable = { json = true, xml = true, csv = true, python = true }
+
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = "*",
+                callback = function(ev)
+                    if not highlight_disable[ev.match] then
+                        pcall(vim.treesitter.start, ev.buf)
+                    end
+
+                    if not indent_disable[ev.match] then
+                        vim.bo[ev.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+                    end
+                end,
+            })
+
+            vim.opt.foldmethod = "expr"
+            vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+            vim.opt.foldenable = false -- don't fold at startup
+            vim.opt.foldlevel = 99 -- when enabled, start with all folds open
         end,
     },
     {
-        "acarapetis/nvim-treesitter-jjconfig",
+        "MeanderingProgrammer/treesitter-modules.nvim",
         dependencies = { "nvim-treesitter/nvim-treesitter" },
-        lazy = false,
-        opts = { ensure_installed = true },
+        opts = {
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<CR>",
+                    scope_incremental = "<CR>",
+                    node_incremental = "<TAB>",
+                    node_decremental = "<S-TAB>",
+                },
+            },
+        },
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        branch = "main",
+        config = function()
+            require("nvim-treesitter-textobjects").setup({
+                select = {
+                    enable = true,
+                    lookahead = true,
+                    keymaps = {
+                        ["af"] = "@function.outer",
+                        ["if"] = "@function.inner",
+                        ["ac"] = "@class.outer",
+                        ["ic"] = "@class.inner",
+                        ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+                    },
+                    selection_modes = {
+                        ["@parameter.outer"] = "v",
+                        ["@function.outer"] = "V",
+                        ["@class.outer"] = "V",
+                    },
+                    include_surrounding_whitespace = false,
+                },
+                swap = {
+                    enable = true,
+                    swap_next = { ["<leader>a"] = "@parameter.inner" },
+                    swap_previous = { ["<leader>A"] = "@parameter.inner" },
+                },
+                move = {
+                    enable = true,
+                    set_jumps = true,
+                    goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer" },
+                    goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer" },
+                    goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer" },
+                    goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer" },
+                },
+            })
+        end,
     },
     {
         "nvim-treesitter/nvim-treesitter-context",
@@ -131,12 +133,10 @@ return {
         keys = {
             { "<Leader>c", vim.cmd.TSContextToggle, desc = "Toggle context" },
             { "[C", function() require("treesitter-context").go_to_context(vim.v.count1) end, desc = "Jump to context" },
-        }
-,
+        },
     },
     {
         "danymat/neogen",
-        -- Uncomment next line if you want to follow only stable versions
         version = "*",
         keys = {
             {
@@ -149,9 +149,7 @@ return {
             enabled = true,
             languages = {
                 python = {
-                    template = {
-                        annotation_convention = "numpydoc",
-                    },
+                    template = { annotation_convention = "numpydoc" },
                 },
             },
         },
